4c1501a6f176a00cf7a7c0784bf84a010108da4e
Tim Dresser <tdresser@gmail.com>
2015-03-01 01:20:34 -0500

    Sort by magnitude.

--- parse-stars.go
+++ parse-stars.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
-	"encoding/csv"	
 	"os"
+	"sort"
 	"strconv"
 )
 
 type Star struct {
-	proper string
-	mag float64
-	x float64
-	y float64
-	z float64
+	Proper string
+	Mag    float64
+	X      float64
+	Y      float64
+	Z      float64
+}
+
+type Stars []Star
+
+func (l Stars) Len() int {
+	return len(l)
+}
+
+func (l Stars) Less(i, j int) bool {
+	return l[i].Mag < l[j].Mag
+}
+
+func (l Stars) Swap(i, j int) {
+	l[i], l[j] = l[j], l[i]
 }
 
 func main() {
@@ -33,26 +48,35 @@ func main() {
 		os.Exit(1)
 	}
 
-	var stars []Star
+	var stars Stars
 
-	for _, row := range rawCSVdata {
+	for i, row := range rawCSVdata {
+		if i == 0 {
+			continue
+		}
+
 		var proper = row[6]
 		var mag, _ = strconv.ParseFloat(row[13], 64)
 		var x, _ = strconv.ParseFloat(row[17], 64)
 		var y, _ = strconv.ParseFloat(row[18], 64)
 		var z, _ = strconv.ParseFloat(row[19], 64)
 		star := Star{
-			proper: proper,
-			mag: mag,
-			x:x,
-			y:y,
-			z:z,
+			Proper: proper,
+			Mag:    mag,
+			X:      x,
+			Y:      y,
+			Z:      z,
 		}
 		stars = append(stars, star)
 	}
 
-	for _, star := range stars {
-		fmt.Printf("%s: %f - (%f, %f, %f)\n", 
-			star.proper, star.mag, star.x, star.y, star.z)
+	sort.Sort(stars)
+
+	for i, star := range stars {
+		if i > 20 {
+			break
+		}
+		fmt.Printf("%s: %f - (%f, %f, %f)\n",
+			star.Proper, star.Mag, star.X, star.Y, star.Z)
 	}
 }
